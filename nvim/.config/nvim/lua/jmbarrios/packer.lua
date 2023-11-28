3303ceebdccb661f51dd4eb76702222731198617
Juan Martín Barrios <j.m.barrios@gmail.com>
2023-11-28 00:14:04 -0600

    update telescope

--- nvim/.config/nvim/lua/jmbarrios/packer.lua
+++ nvim/.config/nvim/lua/jmbarrios/packer.lua
@@ -2,7 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
